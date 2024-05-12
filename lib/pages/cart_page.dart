26541f82ac687570556645befcff4bb8c6f27afd
codewithjalloh <codewithjalloh@gmail.com>
2024-05-12 19:08:47 +0100

    app features done

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_three_ecommerce/components/my_button.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -52,6 +53,46 @@ class CartPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
+        elevation: 0,
+        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Cart Page"),
+      ),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Column(
+        children: [
+          // cart list
+          Expanded(
+            child: cart.isEmpty
+                ? Center(
+                    child: Text("Your cart is empty"),
+                  )
+                : ListView.builder(
+                    itemCount: cart.length,
+                    itemBuilder: (conext, index) {
+                      // get individual item in cart
+                      final item = cart[index];
+
+                      // return as a cart
+                      return ListTile(
+                        title: Text(item.name),
+                        subtitle: Text(item.price.toStringAsFixed(2)),
+                        trailing: IconButton(
+                          icon: Icon(Icons.remove),
+                          onPressed: () => removeItemFromCart(conext, item),
+                        ),
+                      );
+                    },
+                  ),
+          ),
+          // pay button
+          Padding(
+            padding: EdgeInsets.all(50),
+            child: MyButton(
+              onTap: () => payButtonPressed(context),
+              child: Text("PAY NOW"),
+            ),
+          )
+        ],
       ),
     );
   }
